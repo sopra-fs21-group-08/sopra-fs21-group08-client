d320a4fc5d1deae4cff894106f10ffb4d90543c5
tobifra <tobias.frauenfelder@uzh.ch>
2021-04-01 18:10:50 +0200

    Removed old components

--- src/components/shared/routers/AppRouter.js
+++ src/components/shared/routers/AppRouter.js
@@ -1,14 +1,8 @@
 import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { LoginGuard } from "../routeProtectors/LoginGuard";
-<<<<<<< HEAD
 import Login from "../../Pages/Login"
 
-=======
-import Login from "../../login/Login";
-import {RegistrationGuard} from "../routeProtectors/RegistrationGuard";
-import Registration from "../../Registration/Registration";
->>>>>>> 48871a623ed6c7da327f2dacbc4ac3905dd64397
 
 class AppRouter extends React.Component {
   render() {
@@ -25,20 +19,7 @@ class AppRouter extends React.Component {
                 </LoginGuard>
               )}
             />
-<<<<<<< HEAD
             <Route path="/" exact render={() => <Redirect to={"/login"} />} />
-=======
-              <Route
-                  path="/registration"
-                  exact
-                  render={() => (
-                      <RegistrationGuard>
-                          <Registration />
-                      </RegistrationGuard>
-                  )}
-              />
-            <Route path="/" exact render={() => <Redirect to={"/game"} />} />
->>>>>>> 48871a623ed6c7da327f2dacbc4ac3905dd64397
           </div>
         </Switch>
       </BrowserRouter>
