dc9ac4adf09f94dcecce8649f0c43e8c2e5b6891
tobifra <tobias.frauenfelder@uzh.ch>
2021-04-02 16:02:40 +0200

    Create a login page and send the input data to the backend. Fixes #57

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
-import { Card } from 'react-bootstrap';
+import { Card, Form, Alert } from 'react-bootstrap';
+import ZButton from '../../views/design/ZButton'
+import Header from '../../views/Header'
+import { api } from '../../helpers/api';
 function Login() {
+    const [user, setUser] = useState({ username: null, password: null })
+    const [alert, setAlert] = useState({ display: false, variant: null, message: null })
+    // Login Functionality
+    const login = async () => {
+        try{
+            const response = await api.put('/login', user);
+
+        }catch(error){
+            console.log(error)
+            setAlert({display: true, variant: "danger", message: error})
+        }
+    }
+
+    const handleUserInputChange = (key, value) => {
+        // Example: if the key is username, this statement is the equivalent to the following one:
+        // this.setState({'username': value});
+        setUser({...user,[key]: value });
+    }
     return (
         <div>
-            <Card className="center">
+            <Header />
+            <Card className="hcenter">
+                <Card.Header className="zbg-1">Login</Card.Header>
                 <Card.Body>
-                    <h1>Welcome to Mr. X Zürich Edition!</h1>
+                {alert.display&&<Alert variant="danger">Testalert</Alert>}
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control type="text" placeholder="Username" onChange={e => {
+                                handleUserInputChange('username', e.target.value);
+                            }}>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control type="password" placeholder="Password"
+                            onChange={e => {
+                                handleUserInputChange('password', e.target.value);
+                            }}>
+                            </Form.Control>
+                        </Form.Group>
+                        <ZButton onClick={login}>Login</ZButton>
+
+                    </Form>
                 </Card.Body>
+                <Card.Footer className="zbg-1">
+                    Don't have an account? Sign up
+                </Card.Footer>
             </Card>
         </div>
     )
