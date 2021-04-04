311c864a02a6bf44f34d787c916be3ca7b149d11
tobifra <tobias.frauenfelder@uzh.ch>
2021-04-04 16:29:54 +0200

    Added Register function

---
+++ src/components/Pages/Register.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import { Card, Form, Alert } from 'react-bootstrap';
+import ZButton from '../../views/design/ZButton'
+import Header from '../../views/Header'
+import { api } from '../../helpers/api';
+import { useHistory, Link } from 'react-router-dom';
+function Register() {
+    const [user, setUser] = useState({ username: null, password: null, controlPassword: null })
+    const [isValid, setIsValid] = useState({ username: true, password: true, controlPassword: true })
+    const [alert, setAlert] = useState({ display: false, variant: null, message: null })
+
+    const history = useHistory();
+    // Register Functionality
+    const register = async () => {
+        try {
+            setIsValid({ ...isValid, password: true, controlPassword: true });
+            if (user.password === user.controlPassword) {
+                const response = await api.put('/register', user);
+                console.log(response)
+            } else {
+                setIsValid({ ...isValid, password: false, controlPassword: false });
+
+            }
+
+        } catch (error) {
+            setAlert({ display: true, variant: "danger", message: error.response.data.message })
+        }
+    }
+
+    const handleUserInputChange = (key, value) => {
+        setUser({ ...user, [key]: value });
+    }
+    return (
+        <div>
+            <Header />
+            <Card className="hcenter zborder">
+                <Card.Header className="zbg-1">Register</Card.Header>
+                <Card.Body>
+                    {alert.display && <Alert variant="danger">{alert.message}</Alert>}
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control type="text" placeholder="Username" onChange={e => {
+                                handleUserInputChange('username', e.target.value);
+                            }} isInvalid={!isValid.username}>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control type="password" placeholder="Password"
+                                onChange={e => {
+                                    handleUserInputChange('password', e.target.value);
+                                }} isInvalid={!isValid.password}>
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Confirm Password:</Form.Label>
+                            <Form.Control type="password" placeholder="Confirm Password"
+                                onChange={e => {
+                                    handleUserInputChange('controlPassword', e.target.value);
+                                }} isInvalid={!isValid.controlPassword} >
+                            </Form.Control>
+                            {!isValid.controlPassword&&<Form.Control.Feedback type="invalid">
+                            Your password doesn't match
+                            </Form.Control.Feedback>}
+                        </Form.Group>
+                        <ZButton disabled={!user.username||!user.password||!user.controlPassword} onClick={register}>Register</ZButton>
+
+                    </Form>
+                </Card.Body>
+                <Card.Footer className="zbg-1">
+                    Do you already have an account? <Link to="/login">Login</Link>
+                </Card.Footer>
+            </Card>
+        </div>
+    )
+}
+
+export default Register
--- src/components/shared/routers/AppRouter.js
+++ src/components/shared/routers/AppRouter.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { LoginGuard } from "../routeProtectors/LoginGuard";
 import Login from "../../Pages/Login"
+import Register from "../../Pages/Register";
+import Background from "../../../views/Background";
+import {Container, Row, Col} from "react-bootstrap";
 
 
 class AppRouter extends React.Component {
@@ -15,7 +18,27 @@ class AppRouter extends React.Component {
               exact
               render={() => (
                 <LoginGuard>
+                  <Container style={{position: "absolute", zIndex: 10}} fluid>
                   <Login />
+                  </Container>
+                  <Background />
+                </LoginGuard>
+              )}
+            />
+            <Route path="/" exact render={() => <Redirect to={"/login"} />} />
+          </div>
+        </Switch>
+        <Switch>
+          <div>
+            <Route
+              path="/register"
+              exact
+              render={() => (
+                <LoginGuard>
+                  <Container style={{position: "absolute", zIndex: 10}} fluid>
+                  <Register />
+                  </Container>
+                  <Background />
                 </LoginGuard>
               )}
             />
