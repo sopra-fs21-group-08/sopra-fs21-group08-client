d5b2e8ee7914fa4de411168bcc9afd5e648ab96f
Zoya Katashinskaya <katashinskaya.zoya@gmail.com>
2021-04-07 15:58:06 +0200

    Rules added

--- src/components/Pages/Lobby.js
+++ src/components/Pages/Lobby.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import { Card, Row, Col, Modal } from 'react-bootstrap'
+
 import Header from '../../views/Header'
-import { Card, Row, Col } from 'react-bootstrap'
 import ZButton from '../../views/design/ZButton'
+import avatar from '../../assets/img/avatar/avatar4.png'
+import Rules from '../../components/shared/Rules'
 
 
 function Lobby() {
     const [players, setTasks] = useState([])
     let gameId = 'Room #1'
     let img = "https://st4.depositphotos.com/4329009/19956/v/600/depositphotos_199564354-stock-illustration-creative-vector-illustration-default-avatar.jpg/100px100"
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     // update list of players every 5 sec
 
@@ -24,7 +31,7 @@ function Lobby() {
                 <Card.Body>
                     <Card.Title>{gameId}</Card.Title>
                     <Row>
-                        <Col><Card.Img variant="top" src={img} /></Col>
+                        <Col><Card.Img variant="top" src={avatar} /></Col>
                         <Col><Card.Img variant="top" src={img} /></Col>
                         <Col><Card.Img variant="top" src={img} /></Col>
                     </Row>
@@ -39,8 +46,28 @@ function Lobby() {
                     <ZButton disabled={players.length < 3}>Start Game</ZButton>
                 </Card.Body>
                 <Card.Footer className="zbg-1">
-                    <Link to="/game">Leave game</Link>
+                    <Row>
+                        <Col><Link to="/game">Leave game</Link></Col>
+                        <Col>
+                            <ZButton onClick={handleShow}>
+                                Rules</ZButton>
+                        </Col>
+                    </Row>
                 </Card.Footer>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Game rules</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Rules />
+                    </Modal.Body>
+                    <Modal.Footer>
+                    <ZButton variant="secondary" onClick={handleClose}>
+                        Close
+                    </ZButton>
+                    </Modal.Footer>
+                </Modal>
 
             </Card>
             
