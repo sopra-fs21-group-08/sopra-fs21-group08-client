8d591258b15d26fb25a4e89f3ff47767105f1b51
tobifra <tobias.frauenfelder@uzh.ch>
2021-04-09 00:03:20 +0200

    Added map

---
+++ src/components/Pages/Map.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup, WMSTileLayer } from 'react-leaflet'
+function Map() {
+    return (
+        <div></div>
+    )
+}
+
+export default Map
