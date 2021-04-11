702bc3de3390a5da420986d73ea1538d88685644
tobifra <tobias.frauenfelder@uzh.ch>
2021-04-11 13:50:48 +0200

    Added OrthoMap of Zürich

--- src/components/Pages/Map.js
+++ src/components/Pages/Map.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, Popup, WMSTileLayer } from 'react-leaflet'
-function Map() {
+import L from 'leaflet';
+function GameMap() {
+    var map;
+    let mapContainer;
+
+    useEffect(() => {
+        map = L.map(mapContainer);
+        map.setView([47.36609056723141, 8.544442200801285], 15);
+        var wmsLayer = L.tileLayer.wms('https://wms.zh.ch/OrthoZHWMS?', {
+            layers: 'ortho_s'
+        }).addTo(map);
+        L.circle([47.36609056723141, 8.544442200801285], {radius: 5}).addTo(map);
+
+
+
+    }, [mapContainer]);
+
+
     return (
-        <div></div>
+        <div className="map-container" style={{ height: "100vh", width: "100vw" }} ref={el => mapContainer = el}>
+
+        </div>
     )
 }
 
-export default Map
+export default GameMap
