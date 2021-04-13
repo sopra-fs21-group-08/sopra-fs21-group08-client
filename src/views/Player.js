5a21d918c2bbd139be7c9195190a263a4a30b0e0
Zoya Katashinskaya <katashinskaya.zoya@gmail.com>
2021-04-13 12:00:39 +0200

    #58 Change routing to lobbies/id

--- src/views/Player.js
+++ src/views/Player.js
@@ -14,7 +14,8 @@ import User from '../components/shared/models/User';
 
 
 /**
- * This is an example of a Functional and stateless component (View) in React. Functional components are not classes and thus don't handle internal state changes.
+ * This is an example of a Functional and stateless component (View) in React. 
+ * Functional components are not classes and thus don't handle internal state changes.
  * Conceptually, components are like JavaScript functions. They accept arbitrary inputs (called “props”) and return React elements describing what should appear on the screen.
  * They are reusable pieces, and think about each piece in isolation.
  * Functional components have to return always something. However, they don't need a "render()" method.
