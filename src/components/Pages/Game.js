b5b946b3132d12abb3a8d1ca9508ef5a00345639
marvinmuenger <marvin.muenger@uzh.ch>
2021-04-15 23:02:20 +0200

    #68 First draft of Sidebar Component

--- src/components/Pages/Game.js
+++ src/components/Pages/Game.js
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 
 import Header from '../../views/Header'
 import Player from '../../views/game/Player'
+import Sidebar from '../../views/game/Sidebar'
 
 const Game = () => {
     const {id} = useParams()
@@ -10,12 +11,7 @@ const Game = () => {
         <div>
             <Header />
             <h1 className="hcenter">This is a game with ID {id}</h1>
-
-            <Player player={1} color={Number(1)}/>
-            <Player player={2} color={Number(2)} isMoving={true}/>
-            <Player player={3} color={Number(3)}/>
-            <Player player={4} color={Number(4)}/>
-
+            <Sidebar />
         </div>
     )
 }
---
+++ src/views/game/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Row, Col, Container } from 'react-bootstrap';
+import Player from './Player'
+
+const Sidebar = () => {
+  return(
+    <div className='sidebar'>
+        <Player player={1} color={Number(1)}/>
+        <Player player={2} color={Number(2)} isMoving={true}/>
+        <Player player={3} color={Number(3)}/>
+        <Player player={4} color={Number(4)}/>
+    </div>
+  );
+}
+
+
+export default Sidebar
