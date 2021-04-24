4ee562e900c6893faa379791450aaa3b2d6f9e65
marvinmuenger <marvin.muenger@uzh.ch>
2021-04-24 23:48:46 +0200

    #46 #47 Fetch messages and send new messages to backend

--- src/views/game/Chat/Chat.js
+++ src/views/game/Chat/Chat.js
@@ -9,17 +9,39 @@ import User from '../../../components/shared/models/User';
 
 const Chat = () => {
     const [user, setuser] = useState([]);
+    const [messages, setmessages] = useState([]);
+    const [chatmessage, setMessage] = useState([]);
+    const [gameID, setgameID] = useState(['1'])
 
     useEffect(() =>
     {
-      const fetchData = async () => {
+      const fetchUser = async () => {
         const response = await api.get('/users/'+localStorage.getItem('userId'));
         console.log(response);
         const user = new User(response.data);
         setuser(user);
       };
-      fetchData();
+      const fetchMessages = async () => {
+        const response = await api.get('/games/'+{gameID}+'/chats')
+        console.log(response);
+        setmessages(response);
+      };
+      fetchUser();
+      fetchMessages();
     },[]);
+
+    const sendMessage = async () => {
+        const requestBody = JSON.stringify({
+          message: chatmessage,
+          userId: localStorage.getItem("userId")
+        });
+        try{
+          await api.post('/games/'+'1'+'/chats', requestBody, { headers: { 'Authorization': localStorage.getItem('token') }});
+        } catch (error) {
+          alert(`Something went wrong while updating the user: \n${handleError(error)}`);
+        }
+        document.getElementById('myInput').value = ''
+    };
 
     return (
         <>
@@ -34,14 +56,20 @@ const Chat = () => {
                 <Accordion.Collapse eventKey="0">
                 <Card.Body style={{paddingLeft: 10, paddingRight: 0, paddingTop: 5,paddingBottom: 5}}>
                 <div style={{height:100, overflowY: 'scroll', overflowX: 'hidden'}}>
-                    <Message text="Hello!" user={user}/>
+                    {messages.map(mess => (
+                     <>
+                     {(() => {
+                        <Message text={mess} user={mess.username}/>
+                       })()}
+                     </>
+                     ))}
                 </div>
                 <Form>
                     <Container style={{ width: '14rem', height: '2rem', marginBottom: '10px', marginTop: '10px', marginLeft: '-15px'}}>
                     <Row>
-                    <Col sm='8'><Form.Control size="sm" style={{ width: '133px'}}>
+                    <Col sm='8'><Form.Control size="sm" style={{ width: '133px'}} id="myInput" onChange={event => setMessage(event.target.value)}>
                     </Form.Control></Col>
-                    <Col style={{marginTop: '2px', marginLeft: '3px'}} ><div style={{ flexDirection: 'row-reverse' }}><EnvelopeFill size={23} style={{ textAlign: 'left' }} /></div></Col>
+                    <Col style={{marginTop: '2px', marginLeft: '3px'}} ><div style={{ flexDirection: 'row-reverse' }}><EnvelopeFill size={23} style={{ textAlign: 'left' }} onClick={() => {sendMessage();}}/></div></Col>
                     </Row>
                     </Container>
                 </Form>
