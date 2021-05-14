7b617d29ddc33be2604ef73a5a78cac88270c1ad
Zoya Katashinskaya <katashinskaya.zoya@gmail.com>
2021-05-15 01:05:42 +0200

    MockDTOs & Avatars files added

---
+++ src/assets/MockDTOs.js
@@ -0,0 +1,43 @@
+export const gameStatus = {
+    "gameId": 3,
+    "currentPlayer": {
+        "user": {
+            "userId": 7,
+            "username": "filip6",
+            "status": "ONLINE",
+            "dob": "1995-07-14",
+            "creationDate": "2021-05-01"
+        },
+        "playerClass": "MRX",
+        "stationId": 219,
+        "wallet": {
+            "train": 10,
+            "tram": 10,
+            "bus": 10,
+            "black": 2,
+            "double": 2
+        }
+    },
+    "currentRound": 1,
+    "gameOver": false,
+    "mrXVisible": false
+}
+
+export const player ={
+    "user": {
+        "userId": 9,
+        "username": "e",
+        "status": "ONLINE",
+        "dob": null,
+        "creationDate": "2021-04-30"
+    },
+    "playerClass": "MRX",
+    "stationId": 145,
+    "wallet": {
+        "bus": 10,
+        "black": 2,
+        "double": 2,
+        "train": 10,
+        "tram": 10
+    }
+}
---
+++ src/views/design/Avatars.js
@@ -0,0 +1,10 @@
+import avatar0 from '../../assets/img/avatar/avatar1.png'
+import avatar1 from '../../assets/img/avatar/avatar2.png'
+import avatar2 from '../../assets/img/avatar/avatar3.png'
+import avatar3 from '../../assets/img/avatar/avatar4.png'
+import avatar4 from '../../assets/img/avatar/avatar5.png'
+import avatar5 from '../../assets/img/avatar/avatar6.png'
+import avatar6 from '../../assets/img/avatar/avatar7.png'
+
+
+export const avatars = [avatar0, avatar1, avatar2, avatar3, avatar4, avatar5, avatar6];
